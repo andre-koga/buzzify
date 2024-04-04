7801709d4c96c0825d921fc72e3bb4d6a60b9e29
S Taubman <akoga3@gatech.edu>
2024-04-04 14:25:08 -0400

    main

--- app/src/main/java/com/example/spotifyacc/MainActivity.java
+++ app/src/main/java/com/example/spotifyacc/MainActivity.java
@@ -25,10 +25,21 @@ public class MainActivity extends AppCompatActivity {
 
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken, mAccessCode;
+
+    public String getAccessToken() {
+        return mAccessToken;
+    }
+
+    public String getAccessCode() {
+        return mAccessCode;
+    }
+
     private Call mCall;
 
     private TextView tokenTextView, codeTextView, profileTextView;
 
+    // This is where the app first loads. It should call the fragment that contains
+    // the login UI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,9 +93,9 @@ public class MainActivity extends AppCompatActivity {
         AuthorizationClient.openLoginActivity(MainActivity.this, AUTH_CODE_REQUEST_CODE, request);
     }
 
-
     /**
-     * When the app leaves this activity to momentarily get a token/code, this function
+     * When the app leaves this activity to momentarily get a token/code, this
+     * function
      * fetches the result of that external activity to get the response from Spotify
      */
     @Override
@@ -108,52 +119,48 @@ public class MainActivity extends AppCompatActivity {
      * This method will get the user profile using the token
      */
     public void onGetUserProfileClicked() {
-//        if (mAccessToken == null) {
-//            Toast.makeText(this, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        // Create a request to get the user profile
-//        final Request request = new Request.Builder()
-//                .url("https://api.spotify.com/v1/me/")
-//                .addHeader("Authorization", "Bearer " + mAccessToken)
-//                .build();
-//
-//        cancelCall();
-//        mCall = mOkHttpClient.newCall(request);
-//
-//        mCall.enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                Log.d("HTTP", "Failed to fetch data: " + e);
-//                Toast.makeText(MainActivity.this, "Failed to fetch data, watch Logcat for more details",
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                try {
-//                    final JSONObject jsonObject = new JSONObject(response.body().string());
-//                    setTextAsync(jsonObject.toString(3), profileTextView);
-//                } catch (JSONException e) {
-//                    Log.d("JSON", "Failed to parse data: " + e);
-//                    Toast.makeText(MainActivity.this, "Failed to parse data, watch Logcat for more details",
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        if (mAccessToken == null) {
+            Toast.makeText(this, "You need to get an access token first!",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("token", mAccessToken);
-        intent.putExtra("code", mAccessCode);
-        startActivity(intent);
+        // Create a request to get the user profile
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me/")
+                .addHeader("Authorization", "Bearer " + mAccessToken)
+                .build();
+
+        cancelCall();
+        mCall = mOkHttpClient.newCall(request);
+
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("HTTP", "Failed to fetch data: " + e);
+                Toast.makeText(MainActivity.this, "Failed to fetch data, watch Logcat for more details",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final JSONObject jsonObject = new JSONObject(response.body().string());
+                    setTextAsync(jsonObject.toString(3), profileTextView);
+                } catch (JSONException e) {
+                    Log.d("JSON", "Failed to parse data: " + e);
+                    Toast.makeText(MainActivity.this, "Failed to parse data, watch Logcat for more details",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     /**
      * Creates a UI thread to update a TextView in the background
      * Reduces UI latency and makes the system perform more consistently
      *
-     * @param text the text to set
+     * @param text     the text to set
      * @param textView TextView object to update
      */
     private void setTextAsync(final String text, TextView textView) {
@@ -169,7 +176,8 @@ public class MainActivity extends AppCompatActivity {
     private AuthorizationRequest getAuthenticationRequest(AuthorizationResponse.Type type) {
         return new AuthorizationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
                 .setShowDialog(false)
-                .setScopes(new String[] { "user-read-email user-top-read " }) // <--- Change the scope of your requested token here
+                .setScopes(new String[] { "user-read-email user-top-read " }) // <--- Change the scope of your requested
+                                                                              // token here
                 .setCampaign("your-campaign-token")
                 .build();
     }
